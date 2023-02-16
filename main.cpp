28a6e47136e0d067c33033d20b2cf831694ff892
Nicolas Arnaud-Cormos <nicolas.arnaudcormos@gmail.com>
2023-02-16 20:46:57 +0100

    Fix QMap:use modern std::map view

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ int main(int argc, char *argv[])
     auto vecString = std::vector<std::string>{"one", "two", "three"};
     auto qvecString = QVector<QString>{"one", "two", "three"};
 
-    auto mapString = std::map<int, QString>{{1, "one"}, {2, "two"}, {3, "three"}};
-    auto qmapString = QMap<int, QString>{{1, "one"}, {2, "two"}, {3, "three"}};
+    auto mapString = std::map<int, QString>{{10, "one"}, {20, "two"}, {30, "three"}};
+    auto qmapString = QMap<int, QString>{{10, "one"}, {20, "two"}, {30, "three"}};
 
-    auto hashString = std::unordered_map<int, QString>{{1, "one"}, {2, "two"}, {3, "three"}};
-    auto qhashString = QHash<int, QString>{{1, "one"}, {2, "two"}, {3, "three"}};
+    auto hashString = std::unordered_map<int, QString>{{10, "one"}, {20, "two"}, {30, "three"}};
+    auto qhashString = QHash<int, QString>{{10, "one"}, {20, "two"}, {30, "three"}};
 
     return 0;
 }
